fb47924d64f716fe0cd262263d405f95bfbe680a
Umang Shrestha <umang@lifesignals.com>
2022-12-16 14:18:23 -0500

    added layout for resume

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  @ViewChild(MatSidenav, { static: true })
+  sidenav!: MatSidenav;
+
+  constructor(
+    private titleService: Title,
+    private observer: BreakpointObserver,
+  ) {
+    this.titleService.setTitle('Portfolio | Umang Shrestha');
+  }
+
+
+  ngOnInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
+  }
+
+
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({ behavior: 'smooth' });
+  }
 }
